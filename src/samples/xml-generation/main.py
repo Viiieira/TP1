7b0c156cdebc0156a573ca9c893d7f34c7af3569
Hugo Vieira <hugo.vieira2002@hotmail.com>
2023-11-22 18:37:09 +0000

    small fix from absolute path to relative path

--- src/samples/xml-generation/main.py
+++ src/samples/xml-generation/main.py
@@ -1,5 +1,5 @@
 from csv_to_xml_converter import CSVtoXMLConverter
 
 if __name__ == "__main__":
-    converter = CSVtoXMLConverter(r"C:\Users\vitor\OneDrive\Ambiente de Trabalho\E.I\IS\TP1\docker\volumes\data\winemag-data_first150k.csv")
+    converter = CSVtoXMLConverter(r"../../../docs/dataset.csv")
     print(converter.to_xml_str())
