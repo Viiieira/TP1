cd8c03b24c8e70c5eb20df1abcce2642740105ef
Vitor Mina <148635045+vitorhmina@users.noreply.github.com>
2023-11-26 17:13:11 +0000

    Get Country Coordinates

--- src/samples/xml-generation/entities/country.py
+++ src/samples/xml-generation/entities/country.py
@@ -1,3 +1,6 @@
+import json
+import urllib.parse
+import urllib.request
 import xml.etree.ElementTree as ET
 
 from entities.wine import Wine
@@ -10,6 +13,26 @@ class Country:
         self._id = Country.counter
         self._name = name
         self._wines = []
+        self._latitude = None
+        self._longitude = None
+        self.fetch_coordinates()
+
+    def fetch_coordinates(self):
+        # Use Nominatim API to fetch coordinates for the country
+        endpoint = "https://nominatim.openstreetmap.org/search"
+        params = {
+            "q": self._name,
+            "format": "json",
+            "limit": 1,
+        }
+        url = f"{endpoint}?{urllib.parse.urlencode(params)}"
+
+        with urllib.request.urlopen(url) as response:
+            data = json.load(response)
+            if data:
+                location = data[0]
+                self._latitude = float(location.get("lat"))
+                self._longitude = float(location.get("lon"))
 
     def add_wine(self, wine: Wine):
         self._wines.append(wine)
@@ -18,6 +41,8 @@ class Country:
         el = ET.Element("Country")
         el.set("id", str(self._id))
         el.set("name", self._name)
+        el.set("latitude", str(self._latitude))  # Convert to string
+        el.set("longitude", str(self._longitude))  # Convert to string
 
         wines_el = ET.Element("Wines")
         for wine in self._wines:
